9c6364e39e52f255175464fb14c935110162815f
Vaibhav Singh <vaibhavsingh411@gmail.com>
2025-01-05 02:14:36 +0530

    Second commit

--- spicejet/src/main/java/vaibhav/Spicejet.java
+++ spicejet/src/main/java/vaibhav/Spicejet.java
@@ -1,7 +1,5 @@
 package vaibhav;
 
-import java.time.Duration;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -24,10 +22,8 @@ public class Spicejet {
         driver.findElement(By.xpath("//div[@data-testid='to-testID-origin']//div[@class='css-1dbjc4n r-14lw9ot r-11u4nky r-z2wwpe r-1phboty r-rs99b7 r-1loqt21 r-13awgt0 r-ymttw5 r-tju18j r-5njf8e r-1otgn73']")).click();
 
         driver.findElement(By.xpath("//div[@class='css-1dbjc4n r-18u37iz']//div[contains(text(), 'Varanasi')]")).click();
-        System.out.println(driver.findElement(By.xpath("//div[@class='css-1dbjc4n r-18u37iz']//div[contains(text(), 'Varanasi')]")).getText());
         Thread.sleep(1000);
         driver.findElement(By.xpath("//div[contains(text(),'Udaipur')]")).click();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
     }
 }
